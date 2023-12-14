9f8baba662ba9245c02d22126c24cdbd211bc04f
Maotao <mzt.live@live.com>
2023-12-15 00:16:28 +0800

    修改mongodb事务回调函数的参数

--- mongo/transaction.go
+++ mongo/transaction.go
@@ -4,14 +4,16 @@ import (
 	"context"
 	"fmt"
 
-	"go.mongodb.org/mongo-driver/mongo"
+	g_mongo "go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readconcern"
 	"go.mongodb.org/mongo-driver/mongo/writeconcern"
 )
 
 // TransactionHandler is a function that will be executed in a transaction
-type TransactionHandler func(ctx mongo.SessionContext, session mongo.Session) (any, error)
+//
+// Must use the database object passed in as a parameter to execute the transaction
+type TransactionHandler func(ctx g_mongo.SessionContext, database *g_mongo.Database) (any, error)
 
 // StartTransaction returns the result of the handler function.
 // If the handler returns an error, the transaction will be aborted.
@@ -23,10 +25,10 @@ type TransactionHandler func(ctx mongo.SessionContext, session mongo.Session) (a
 //		// do something
 //		return nil, nil
 //	})
-func StartTransaction(ctx context.Context, db *mongo.Database, handler TransactionHandler) (any, error) {
+func StartTransaction(ctx context.Context, db *g_mongo.Database, handler TransactionHandler) (any, error) {
 	var (
 		result  any
-		session mongo.Session
+		session g_mongo.Session
 		err     error
 	)
 
@@ -37,8 +39,8 @@ func StartTransaction(ctx context.Context, db *mongo.Database, handler Transacti
 	}
 	defer session.EndSession(ctx)
 
-	result, err = session.WithTransaction(ctx, func(sCtx mongo.SessionContext) (interface{}, error) {
-		return handler(sCtx, session)
+	result, err = session.WithTransaction(ctx, func(sCtx g_mongo.SessionContext) (interface{}, error) {
+		return handler(sCtx, db)
 	}, txo)
 
 	return result, err
